8f00106242334c9ce5c601484a3e59272ffe5b5d
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-08 02:26:43 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end 
   
   def create 
-    return redirect_to(controller: 'sessions', action: 'new')
+    return redirect_to(controller: 'sessions', action: 'new') if !params[:name]
     session[:name] = params[:name]
     redirect_to controller: 'application', action: 'hello'
   end 
