0937292cfb00fab4354a65ebc5f756747fca3c1c
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-08 02:25:10 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   
   def create 
     session[:name] = params[:name]
+    redirect_to controller: 'application', action: 'hello'
   end 
   
   def destroy 
